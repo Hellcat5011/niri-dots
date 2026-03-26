9e642c765310ed1bada707af38c28ae8afbbd677
hellcat5011 <vicjyrobyn@live.com>
2026-03-27 04:01:27 +0530

    first commit

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+sudo pacman -S git base-devel --needed
+
+dir=$(pwd)
+
+#paru
+if ! pacman -Qs paru > dev/null; then
+mkdir $HOME/paru
+git clone https://aur.archlinux.org/paru.git $HOME/paru
+cd $HOME/paru
+makepkg -si
+
+cd $dir
+
+fi
+
+paru -S --needed $(cat ./progs.txt)
+
+mkdir -p "$HOME/.config"
+
+cp -R ./configs/* $HOME/.config/
+
+sudo sed -i "s/^MODULES=(.*/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/" "/etc/mkinitcipo.conf"
+
+sudo mkinitcpio -P
+
+sudo cp ./sddm.conf /etc/sddm.conf
+
+sleep 2
+
+mkdir -p $HOME/.local/share/fonts
+cp -r ./fonts/* $HOME/.local/share/fonts/
+fc-cache -f -v
+
+sudo systemctl enable sddm
+sudo systemctl enable NetworkManager
+
+echo "Install complete, please reboot your system"
+read -p "Press enter to exit ..."

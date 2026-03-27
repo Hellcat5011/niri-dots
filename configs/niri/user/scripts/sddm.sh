8f774e78415034d153e30edf24e936838dce5905
hellcat5011 <vicjyrobyn@live.com>
2026-03-27 19:21:48 +0530

    streamlining setup and configs

--- configs/niri/user/scripts/sddm.sh
+++ configs/niri/user/scripts/sddm.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+current=$(qs -c noctalia-shell ipc call wallpaper get "all")
+
+
 choose() {
 
   local choice=$1
 
   if [[ $choice == [yY]* ]]; then
     {
-      sudo cp -f '/home/vic/.config/niri/user/scripts/.wallpaper_current' '/usr/share/sddm/themes/sugar-candy/Backgrounds/default'
+      sudo cp -f "$current" '/usr/share/sddm/themes/sugar-candy/Backgrounds/default'
       echo "Done! Press enter to exit"
       read
     }
